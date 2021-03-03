75c3fb36bdfe354b88ec03169db938c0e2d8f1c8
Maxim Vinokurov <vmstr8@yandex.ru>
2021-03-03 04:39:49 +0300

    Добавлен файл с параметрами xpath.

---
+++ xpath.py
@@ -0,0 +1,19 @@
+XPATH_ENTIRE_EVENT_BLOCK = '/html/body/div[1]/div[2]/div[2]/div[1]/div[3]/div[1]/div/div/div[1]/div/div[1]/div[2]/div[' \
+                           '2]/div/div[3]/div[10]/div[1]/form/div/div/div/div[7]/div[2]/div[2]'
+
+XPATH_PERMANENT_LINK = '/html/body/div[1]/div[2]/div[2]/div[1]/div[3]/div[1]/div/div/div[1]/div/div[1]/div[2]/div[' \
+                       '2]/div/div[3]/div[3]/h2/button'
+
+XPATH_ORGANIZATION_LINK = '/html/body/div[1]/div[2]/div[2]/div[1]/div[3]/div[1]/div/div/div[1]/div/div[1]/div[2]/div[' \
+                          '2]/div/div[3]/div[4]/h2/button '
+
+XPATH_ORGANIZATION_CLASS = 'editor-post-taxonomies__hierarchical-terms-filter'
+
+XPATH_ORGANIZATION_CHECKBOX = '/html/body/div[1]/div[2]/div[2]/div[1]/div[3]/div[1]/div/div/div[1]/div/div[1]/div[' \
+                              '2]/div[2]/div/div[3]/div[4]/div/div/div/div/label '
+
+XPATH_IMG_LINK = '/html/body/div[1]/div[2]/div[2]/div[1]/div[3]/div[1]/div/div/div[1]/div/div[1]/div[2]/div[' \
+                 '2]/div/div[3]/div[6]/h2/button '
+
+XPATH_DESCRIPTION_LINK = '/html/body/div[1]/div[2]/div[2]/div[1]/div[3]/div[1]/div/div/div[1]/div/div[1]/div[2]/div[' \
+                         '2]/div/div[3]/div[7]/h2/button '
